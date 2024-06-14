fe48e3ce6212cec8c015bfedd3a4f2f773a21648
Rafael <rafa92muhitdinov@gmail.com>
2024-06-14 11:38:36 +0500

    save3

--- modules/cash-register/them-1/CashRegister.jsx
+++ modules/cash-register/them-1/CashRegister.jsx
@@ -1,19 +1,17 @@
 import Kassa from "@/modules/cash-register/them-1/components/kassa/Kassa";
 import Navbar from "@/modules/cash-register/them-1/components/header/Navbar";
 import Table from "@/modules/cash-register/them-1/components/Table";
-
+import './Them-1.scss'
 export default function CashRegister() {
   return (
-    <main className="flex">
-      <div className="containerKassa flex gap-[5px]">
-        <section className="left w-[80%]">
+    <main className="flex cashRegister">
+        <section className="cashRegister__left !w-[80%]">
           <Navbar />
           <Table />
         </section>
-        <section className="right w-[20%]">
+        <section className="cashRegister__right !w-[20%]">
           <Kassa />
         </section>
-      </div>
     </main>
   );
 }
